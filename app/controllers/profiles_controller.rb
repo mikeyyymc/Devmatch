6acba2537c9cfec945fb6ed03ee498caffa85878
Michael McCown <mikea.s.c@gmail.com>
2017-06-03 11:13:40 +0000

    Added users controller and show view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,22 +6,21 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
   
-  # POST to /users/:user_id/profile
-  def create
-    # Ensure that we have the user who is filling out form
-    @user = User.find( params[:user_id] )
-    # Create profile linked to this specific user
-    @profile = @user.build_profile( profile_params )
-    if @profile.save
-      flash[:success] = "Profile updated!"
-      redirect_to root_path
-    else
-      render action :new
-    end
+# POST to /users/:user_id/profile
+def create
+  # Ensure that we have the user who is filling out form
+  @user = User.find( params[:user_id] )
+  # Create profile linked to this specific user
+  @profile = @user.build_profile( profile_params )
+  if @profile.save
+    flash[:success] = "Profile updated!"
+    redirect_to root_path
+  else
+    render action: :new
   end
-  
-  private
-    def profile_params
-      params.required(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-    end
+end
+private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  end
 end
